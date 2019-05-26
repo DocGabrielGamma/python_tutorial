7bbd7abedccea8c41c7f5ffd3c7ccda1082a6ad5
Elio Freitas <samuelgf92@gmail.com>
2019-05-26 17:01:33 +0100

    Merging excel files

--- Modules/pathReader.py
+++ Modules/pathReader.py
@@ -3,9 +3,10 @@ import sys, getopt
 def getFilePaths(arguments):
    inputPath = ''
    outputPath = ''
+   columnHeader = ''
    try:
       # h:Help , i: for input, o: for ouput
-      opts, args = getopt.getopt(arguments,"hi:o:",["iPath=","oPath="]) 
+      opts, args = getopt.getopt(arguments,"hi:o:c:",["iPath=","oPath=","column="]) 
    except getopt.GetoptError: 
       print ('Error incorrect arguments format it should be \n excelMerger.py -i <inputfile> -o <outputfile>')
       sys.exit(2)
@@ -17,4 +18,6 @@ def getFilePaths(arguments):
          inputPath = arg
       elif opt in ("-o", "--oPath"):
          outputPath = arg
-   return (inputPath, outputPath)
+      elif opt in ("-c", "--column"):
+         columnHeader = arg
+   return (inputPath, outputPath, columnHeader)
--- excelMerger.py
+++ excelMerger.py
@@ -1,25 +1,62 @@
 #!/usr/bin/python
 import sys, getopt
 from pathlib import Path
-from Readers import excelReader, pathReader
+from openpyxl import Workbook, load_workbook
+from Modules import excelReader, pathReader, groupReader
 
-def getDifferentManagersInTable(table):
-    managers = ["1113", "5166", "515"]
-    return managers
+def createExcelInPath(path):
+    wb = Workbook()
+    wb.save(path)
+    return wb
+
+def createFileIfRequired(name, outputPath):
+    excelName = name + ".xlsx"
+    excelFilesNames = excelReader.getExcelFileNamesinPath(outputPath)
+    finalPath = outputPath + '\\' + excelName
+    if len(excelFilesNames) < 1:
+        return createExcelInPath(finalPath), finalPath
+    for excelFileName in excelFilesNames:
+        if excelFileName.find(excelName) > -1:
+           return load_workbook(finalPath), finalPath
+    return createExcelInPath(finalPath), finalPath
+    
+def createFileByGroups(groups, file, outputPath):
+    for group in groups:
+      excelFile, path = createFileIfRequired(group, outputPath)
+      rows = extractAllRowsWithGroup(file, group)
+      appendRowsToTable(file, excelFile, path, rows)
+    return "lastRow"
 
 def createExcelTables(managers):
     excelTables = managers
     return excelTables
 
-def extractAllRowsFromTableWithManagerId(table, managerID):
-    return "rows"
+def extractAllRowsWithGroup(file, group):
+    return groupReader.findRowWithValue(group, file.active)
 
-def appendRowsToTable(table, rows):
-    return "sucessfull"
+def appendRowsToTable(inputFile, outputFile, path, rows, offsetRow = 0, offsetColumn = 1):
+    sheet = outputFile.active
+    initialRow = sheet.max_row
+    for rowIndex, keys in enumerate(rows):
+        for columnIndex, value in enumerate(rows[keys]):
+            rowCoordinate = rowIndex + initialRow + offsetRow
+            columnCoordinate = columnIndex + offsetColumn
+            sheet.cell(row=rowCoordinate, column=columnCoordinate, value=value)
+    outputFile.save(path)
+    #printTables(inputFile, outputFile, path, rows)
+    return "Success"
+
+def createExcelFileForGroup(inputFiles, outputPath, columnHeader):
+    groups = []
+    for file in inputFiles:
+        groups = groupReader.getDifferentGroupsFile(file, columnHeader)
+        createFileByGroups(groups, file, outputPath)
+    return groups
 
 def main(arguments):
-    inputPath, outputPath = pathReader.getFilePaths(arguments)
-    excelFiles = excelReader.openExcelAt(inputPath)
+    inputPath, outputPath, columnHeader = pathReader.getFilePaths(arguments)
+    inputExcelFiles = excelReader.openExcelAt(inputPath)
+    createExcelFileForGroup(inputExcelFiles, outputPath, columnHeader)
     print(inputPath)
     print(outputPath)
 
